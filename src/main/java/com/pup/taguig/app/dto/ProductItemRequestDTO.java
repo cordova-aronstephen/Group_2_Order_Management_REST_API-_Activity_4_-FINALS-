5c6951e42e25747520beabc6d8e003dbee43ade9
cordova-aronstephen <aronstephenscordova@gmail.com>
2026-06-10 13:39:21 +0800

    Add product items request dto

---
+++ src/main/java/com/pup/taguig/app/dto/ProductItemRequestDTO.java
@@ -0,0 +1,13 @@
+package com.pup.taguig.app.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductItemRequestDTO {
+	
+	private Long productId;
+	private int quantity;
+	
+}
