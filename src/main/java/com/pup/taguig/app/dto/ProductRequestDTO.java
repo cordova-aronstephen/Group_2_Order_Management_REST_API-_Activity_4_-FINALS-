8336b40eb1d7ffef3651c7ee99f719c22f90bff4
caya-karlchristian <karlcaya0704@gmail.com>
2026-06-12 19:43:09 +0800

    Update product request dto

--- src/main/java/com/pup/taguig/app/dto/ProductRequestDTO.java
+++ src/main/java/com/pup/taguig/app/dto/ProductRequestDTO.java
@@ -2,6 +2,10 @@ package com.pup.taguig.app.dto;
 
 import java.math.BigDecimal;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,9 +13,17 @@ import lombok.Setter;
 @Setter
 public class ProductRequestDTO {
 	
+	@NotBlank(message = "Product name is required")
 	private String name;
+	
 	private String description;
+	
+	@NotNull(message = "Price is required")
+	@DecimalMin(value = "0.01", message = "Price must be greater than 0")
 	private BigDecimal price;
+	
+	@NotNull(message = "Stock is required")
+    @Min(value = 0, message = "Stock must be 0 or greater")
 	private Integer stock;
 	
 }
