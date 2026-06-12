808d32bdde56ad836d738dc1a882733bbdab9c3f
caya-karlchristian <karlcaya0704@gmail.com>
2026-06-12 19:57:06 +0800

    Update product mapper in repository

--- src/main/java/com/pup/taguig/app/repository/ProductMapper.java
+++ src/main/java/com/pup/taguig/app/repository/ProductMapper.java
@@ -1,8 +1,20 @@
 package com.pup.taguig.app.repository;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.pup.taguig.app.model.Product;
 
 @Mapper
 public interface ProductMapper {
-
+	
+	public void insertProduct(Product product);
+	public Product findById(Long id);
+	public List<Product> findAllProducts();
+	public int countAllProducts();
+	public void decreaseStock(@Param("productId") Long productId,
+							 @Param("quantity") int quantity);
+	
 }
