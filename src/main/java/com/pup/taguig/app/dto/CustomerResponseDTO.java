df38ab8cc10d6dd7eceb6624907b6ea086fb6d52
cordova-aronstephen <aronstephenscordova@gmail.com>
2026-06-13 18:43:29 +0800

    Remove the attributes to hide sensitive information

--- src/main/java/com/pup/taguig/app/dto/CustomerResponseDTO.java
+++ src/main/java/com/pup/taguig/app/dto/CustomerResponseDTO.java
@@ -2,84 +2,19 @@ package com.pup.taguig.app.dto;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerResponseDTO {
 
-    private int id;
     private String firstName;
     private String lastName;
     private String middleName;
-    private String email;
-    private String phone;
-    private LocalDateTime createdAt;
 
-    public CustomerResponseDTO() {
-    }
-
-    public CustomerResponseDTO(int id, String firstName, String lastName, String middleName, String email,
-            String phone, LocalDateTime createdAt) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.middleName = middleName;
-        this.email = email;
-        this.phone = phone;
-        this.createdAt = createdAt;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-    
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-    
-    public String getMiddleName() {
-        return middleName;
-    }
-    
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-    
-    public String getEmail() {
-        return email;
-    }
-    
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    
-    public String getPhone() {
-        return phone;
-    }
-    
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-    
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-    
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
 }
